ee0fd87cfc5fb2e2e1b796e4c4eeff7bb265b0ef
rebeccanorth <rn256@cornell.edu>
2017-11-10 11:50:53 -0500

    Photogallery

---
+++ photogallery.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html> <!--rn256, erd44, jnp49-->
+<html>
+<head>
+  <meta charset="UTF-8">
+  <link rel="stylesheet" type="text/css" href="Styles/all.css" media="all"/>
+  <title>OffTheChain - photos</title>
+
+</head>
+<body>
+  <p> hi </p>
+  <p> testing  </p>
+
+</body>
+
+</html>

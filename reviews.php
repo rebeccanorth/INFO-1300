09bb50312fad1dc1e3f0d7dada7e12a1900da459
Eoghan Richard Dempsey <erd44@cornell.edu>
2017-11-12 19:39:28 -0500

    rev

--- reviews.php
+++ reviews.php
@@ -11,16 +11,16 @@
     <?php $page = 'reviews'; include("includes/navigation.php");?>
   </header>
 
-  <p> "I wear it all the time, I simply love it!" </p>
-  <p> "I am in love with my necklace, I've been looking for a necklace like this for so long I'm definitely glad I came across this shop. The letters did fade quickly but I don't really mind it."</p>
-  <p> "Wonderful product, amazing shop. The owner was very kind and available for questions and support. I would recommend this product and this shop."</p>
-  <p> "Great necklace! It is a perfect length and will look cute with anything. Highly recommend!" </p>
-  <p> "In LOVE with my new necklace!!! The quality is great, the service was perfect, and it was sent so quickly!!!! Thank you!!!!" </p>
-  <p> "I got the gold finish with 6 letters-- It was better quality than I expected and came super fast! Looks great layered. Would definitely buy again." </p>
-  <p> "I love my necklace so much. It came quickly and looks great!" </p>
-  <p> "This necklace was absolutely amazing! I had seen so many similar necklaces but they were so expensive! These are affordable and great quality! Great layering piece!" </p>
-  <p> "I love it! It's super cute, but my letters turned to a rose gold color after only wearing it 3 times. I didn't shower with it or get it wet either." </p>
-  <p> "Love this choker! Would definitely order again." </p>
+  <p> "I wear it all the time, I simply love it!" - Mélina</p>
+  <p> "I am in love with my necklace, I've been looking for a necklace like this for so long I'm definitely glad I came across this shop. The letters did fade quickly but I don't really mind it." - Gabriella</p>
+  <p> "Wonderful product, amazing shop. The owner was very kind and available for questions and support. I would recommend this product and this shop." - Zacharias </p>
+  <p> "Great necklace! It is a perfect length and will look cute with anything. Highly recommend!" - Haley</p>
+  <p> "In LOVE with my new necklace!!! The quality is great, the service was perfect, and it was sent so quickly!!!! Thank you!!!!" - Rachel</p>
+  <p> "I got the gold finish with 6 letters-- It was better quality than I expected and came super fast! Looks great layered. Would definitely buy again." - Elizabeth </p>
+  <p> "I love my necklace so much. It came quickly and looks great!" - Callie </p>
+  <p> "This necklace was absolutely amazing! I had seen so many similar necklaces but they were so expensive! These are affordable and great quality! Great layering piece!" - Hannah</p>
+  <p> "I love it! It's super cute, but my letters turned to a rose gold color after only wearing it 3 times. I didn't shower with it or get it wet either." - Jade</p>
+  <p> "Love this choker! Would definitely order again." - Harrison</p>
 
 </body>
 

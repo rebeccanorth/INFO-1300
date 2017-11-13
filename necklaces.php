167e4c6bac9336e05b49d3b302274827f5f71013
Jessica Prague <jnprag1@aol.com>
2017-11-12 19:45:40 -0500

    Necklaces

--- necklaces.php
+++ necklaces.php
@@ -10,6 +10,130 @@
   <header>
     <?php $page = 'necklaces'; include("includes/navigation.php");?>
   </header>
+  <!-- Necklace 1 -->
+  <div class="necklaces">
+    <img src="images/Personalized Name Choker.jpg" alt="personalized">
+    <div class="description-overlay">
+      <p> Personalized Name Necklace/Choker (Small Letters) </p>
+      <ul>
+        <li>Silver or gold tone chain</li>
+        <li>1/4 in (.25 inch) silver or gold tone uppercase letters</li>
+        <li>14 inch adjustable chain (also comes in different sizes)</li>
+        <li>Not only just for names, but initials and words look great!</li>
+      </ul>
+    </div>
+    <div class="price-overlay">
+      <p class="price"> $30 (0-6 letters) <br> $34 (7-10 letters) <br> $37 (11+ letters)</p>
+    </div>
+  </div>
+
+  <!-- Necklace 2 -->
+  <div class="necklaces">
+    <img src="images/Double Chain Customized Name Necklace.jpg" alt="double">
+    <div class="description-overlay">
+      <p> Double Chain Customized Name Necklace </p>
+      <ul>
+        <li>Silver or gold tone chains</li>
+        <li>1/4 in (.25 inch) silver or gold tone uppercase letters or 1/2 in silver or gold tone letters</li>
+        <li>14-16 inch adjustable chain (Longer chain will be an inch longer)</li>
+        <li>Not only just for names, but initials and words look great!</li>
+      </ul>
+    </div>
+    <div class="price-overlay">
+      <p class="price"> $45 (0-12 letters) <br> $50 (13-18 letters) <br> $60 (18+ letters)</p>
+    </div>
+  </div>
+
+  <!-- Necklace 3 -->
+  <div class="necklaces">
+    <img src="images/Personalized Gold Plated Name Necklace.jpg" alt="double">
+    <div class="description-overlay">
+      <p> Personalized Gold Plated Name Necklace/Choker </p>
+      <ul>
+        <li>Gold plated uppercase letters 7 x 10 mm</li>
+        <li>14 inch adjustable chain (also comes in different sizes)</li>
+        <li>Can be worn as a choker or a short necklace</li>
+        <li>Not only just for names, but initials and words look great!</li>
+      </ul>
+    </div>
+    <div class="price-overlay">
+      <p class="price"> $40 (0-6 letters) <br> $45 (7-10 letters) <br> $50 (11+ letters)</p>
+    </div>
+  </div>
+
+  <!-- Necklace 4 -->
+  <div class="necklaces">
+    <img src="images/Initial Lariat Necklace.jpg" alt="initial">
+    <div class="description-overlay">
+      <p> Initial Lariat Necklace </p>
+      <ul>
+        <li>Gold or silver tone lariat necklace</li>
+        <li>Adjustable 17 inch chain (also comes in different sizes)</li>
+        <li>Letter chain is 3.5 inches</li>
+        <li>Perfect for dressing up for a fancy occasion, or everyday!</li>
+      </ul>
+    </div>
+    <div class="price-overlay">
+      <p class="price"> $30 </p>
+    </div>
+  </div>
+
+  <!-- Necklace 5 -->
+  <div class="necklaces">
+    <img src="images/jesse-large.JPG" alt="large">
+    <div class="description-overlay">
+      <p> Personalized Name Necklace (Large Letters) </p>
+      <ul>
+        <li>Silver or gold tone chains</li>
+        <li>1/2 inch (0.5 in) silver tone uppercase letters</li>
+        <li>14 inch adjustable chain (also comes in different sizes)</li>
+        <li>Can be worn as a choker or a short necklace</li>
+        <li>Not only just for names, but initials and words!</li>
+      </ul>
+    </div>
+    <div class="price-overlay">
+      <p class="price"> #30 (0-6 letters) <br> $34 (7-10 letters) <br> $37 (11+ letters)</p>
+    </div>
+  </div>
+
+  <!-- Necklace 6 -->
+  <div class="necklaces">
+    <img src="images/Beaded Chain.jpg" alt="large">
+    <div class="description-overlay">
+      <p> Personalized Beaded Chain Name Necklace/Choker </p>
+      <ul>
+        <li>Gold plated 2mm black beaded square chain</li>
+        <li>Gold plated upper case letters 7 x 10 mm</li>
+        <li>14 inch adjustable chain (also comes in different sizes)</li>
+        <li>Can be worn as a choker or a short necklace</li>
+        <li>Not only just for names, but initials and words!</li>
+      </ul>
+    </div>
+    <div class="price-overlay">
+      <p class="price"> #50 (0-6 letters) <br> $55 (7-10 letters) <br> $60 (11+ letters)</p>
+    </div>
+  </div>
+
+  <!-- Necklace 7 -->
+  <div class="necklaces">
+    <img src="images/Disc Choker.jpg" alt="large">
+    <div class="description-overlay">
+      <p> Personalized Disc Choker Name Necklace </p>
+      <ul>
+        <li>Gold Plated Disc Chain</li>
+        <li>Gold Plated Uppercase Letters 7 x 10 mm</li>
+        <li>3 Different Chain Lengths:  12in, 13in, or 14in</li>
+        <li>Can be worn as a choker or a short necklace</li>
+        <li>Not only just for names, but initials and words!</li>
+      </ul>
+    </div>
+    <div class="price-overlay">
+      <p class="price"> #50 (0-6 letters) <br> $55 (7-10 letters) <br> $60 (11+ letters)</p>
+    </div>
+  </div>
+
+
+
 </body>
 
 </html>

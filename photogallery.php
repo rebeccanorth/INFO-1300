edb390453357dbe83fde7e3ebb0339fdf262784c
rebeccanorth <rn256@cornell.edu>
2017-11-14 12:35:55 -0500

    Photogallery
    Added pictures

--- photogallery.php
+++ photogallery.php
@@ -13,7 +13,37 @@
     <?php $page = 'gallery'; include("includes/navigation.php");?>
   </header>
 
-  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+    <img class="pix" src="images/bas.JPG" alt="bas">
+    <img class="pix" src="images/bodychain.JPG" alt="bodychain">
+    <img class="pix" src="images/cam.JPG" alt="cam">
+    <img class="pix" src="images/circle.JPG" alt="circle">
+    <img class="pix" src="images/dg.JPG" alt="dg">
+    <img class="pix" src="images/dope.JPG" alt="dope">
+    <img class="pix" src="images/emily.JPG" alt="emily">
+    <img class="pix" src="images/goblue.JPG" alt="goblue">
+    <img class="pix" src="images/hookem.JPG" alt="hookem">
+    <img class="pix" src="images/jamie.JPG" alt="jamie">
+    <img class="pix" src="images/jenna.JPG" alt="jenna">
+    <img class="pix" src="images/jesse.JPG" alt="jesse">
+    <img class="pix" src="images/jw.JPG" alt="jw">
+    <img class="pix" src="images/kelsey.JPG" alt="kelsey">
+    <img class="pix" src="images/kkg.JPG" alt="kkg">
+    <img class="pix" src="images/love.JPG" alt="love">
+    <img class="pix" src="images/love2.JPG" alt="love2">
+    <img class="pix" src="images/lovejimmy.JPG" alt="lovejimmy">
+    <img class="pix" src="images/pearlbodychain.JPG" alt="pearlbodychain">
+    <img class="pix" src="images/sarah.JPG" alt="sarah">
+    <img class="pix" src="images/star.JPG" alt="star">
+    <img class="pix" src="images/stone.JPG" alt="stone">
+    <img class="pix" src="images/szpck.JPG" alt="szpck">
+    <img class="pix" src="images/valerie.JPG" alt="valerie">
+
+
+
+
+
+
+  <!-- <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
   <ol class="carousel-indicators">
     <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
     <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
@@ -38,7 +68,7 @@
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
-</div>
+</div> -->
 </body>
 
 </html>

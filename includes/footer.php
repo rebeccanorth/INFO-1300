a22d0b0c32b935722ebe124fdcbafedcc6cede68
Jessica Prague <jnprag1@aol.com>
2017-12-07 16:28:55 -0500

    Footer shadow

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,8 @@
 <div class = "footer">
   <p id="follow"> Follow me on: </p> <br>
   <div>
-    <a href="https://www.facebook.com/offthechainbyjesse/"><img class="social" src="images/facebook.png" alt="fb">
-    <a href="https://www.instagram.com/offthechainbyjesse/"><img class="social" src="images/instagram.png" alt="insta">
-    <a href="https://www.etsy.com/shop/offthechainbyjesse"><img class="social" src="images/etsy.png" alt="etsy">
+    <a class="social_link" href="https://www.facebook.com/offthechainbyjesse/"><img class="social" src="images/facebook.png" alt="fb">
+    <a class="social_link" href="https://www.instagram.com/offthechainbyjesse/"><img class="social" src="images/instagram.png" alt="insta">
+    <a class="social_link" href="https://www.etsy.com/shop/offthechainbyjesse"><img class="social" src="images/etsy.png" alt="etsy">
   </div>
 </div>
